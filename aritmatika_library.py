dd140143f41b5e4724679e9b18c0e0345e77d93b
AlbertWij <92645507+AlbertWij@users.noreply.github.com>
2023-10-09 07:33:32 +0700

    Delete aritmatika_library.py

--- aritmatika_library.py
+++
@@ -1,8 +0,0 @@
-import math
-
-a = 10
-b = 15
-c = math.sqrt(pow(a,2)+pow(b,2))
-print("Alas segitiga siku-siku =", a)
-print("Tinggi segitiga siku-siku =", b)
-print("Sisi miring segitiga siku-siku =", c)
